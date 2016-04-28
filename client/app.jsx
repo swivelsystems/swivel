fce60fd0e6545e6cd8635f621661e0228fcd28ff
Kevin E Meraz <kevinemeraz@gmail.com>
2016-04-28 12:35:47 -0700

    add first commit.

---
+++ client/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return <h1>Hello, World</h1>;
+  }
+
+}
+
+export default App;
