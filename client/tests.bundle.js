a05cf7bd11c84cf121e49b187956e4d5de2145d0
Joel Aguero <joelmaguero@gmail.com>
2016-04-29 18:42:17 -0700

    Attempt to fix bug

--- client/tests.bundle.js
+++ client/tests.bundle.js
@@ -1,4 +1,3 @@
 // Make sure you have your directory and regex test set correctly!
 const context = require.context('./tests', true, /.tests\.js$/);
 context.keys().forEach(context);
-module.exports = context;
--- client/tests/app.tests.js
+++ client/tests/app.tests.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const TestUtils = require('react-addons-test-utils'); // Alternately could use the DOM API
 
-import App from '/../components/App.jsx';
+import App from '../components/App.jsx';
 
 describe('app', () => {
   it('renders without problems', () => {
