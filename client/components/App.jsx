e8b67c9364f81bd9191592ba508b75c1f613a76d
Joel Aguero <joelmaguero@gmail.com>
2016-04-30 17:19:39 -0700

    Add client dir

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,66 +1,17 @@
 import React from 'react';
-import SideBar from './SideBar.jsx';
-import Main from './Main.jsx';
-import Nav from './Nav.jsx';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      courses: [
-        {
-          name: 'HistoryFall2015',
-          students: ['jack', 'bob', 'james'],
-          assignments: ['Midterm', 'The Past 100 Years in the Middle East', 'The World Rulers'] },
-        {
-          name: 'EnglishFall2015',
-          students: ['jack', 'amy', 'james'],
-          assignments: ['Final', 'Hemingway and other Ex-Pats', 'The World of the Great Gastby'] },
-        {
-          name: 'TrigonometryFall2015',
-          students: ['lizzy', 'laura', 'andrew'],
-          assignments: ['Quiz1', 'Homework1', 'Homework2'] },
-      ],
-      currentCourse: {},
-      isHome: true,
-      courseChanged: false,
     };
   }
 
-  handleClass(course) {
-    this.setState({ currentCourse: course });
-    this.setState({ isHome: false });
-    this.setState({ courseChanged: true });
-  }
-
-  handleHome() {
-    this.setState({ isHome: true });
-  }
-
   render() {
-    return (
-      <div>
-        <div className="row">
-          <Nav />
-        </div>
-        <div className="row">
-          <SideBar
-            handleClass={this.handleClass.bind(this)}
-            handleHome={this.handleHome.bind(this)}
-            courses={this.state.courses}
-          />
-          <Main
-            isHome={this.state.isHome}
-            currentCourse={this.state.currentCourse}
-            courseChanged={this.state.courseChanged}
-          />
-        </div>
-      </div>
-    );
+    return <h1>Swivel swivel </h1>;
   }
 
 }
-
 
 export default App;
