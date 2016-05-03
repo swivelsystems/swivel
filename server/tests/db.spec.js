4715a94acbfb50ade2f1ad0291f62e49e0e17345
Joel Aguero <joelmaguero@gmail.com>
2016-05-02 18:36:35 -0700

    Write shells for server unit tests

---
+++ server/tests/db.spec.js
@@ -0,0 +1,129 @@
+const request = require('request');
+
+describe('Database', function() {
+  describe('Students', function() {
+    it('should create new students', (done) => {
+
+    });
+
+    it('should successfully lookup existing students', (done) => {
+
+    });
+
+    it('should update existing students', (done) => {
+
+    });
+
+    it('should remove students from the database', (done) => {
+
+    });
+  });
+
+  describe('Teachers', function() {
+    it('should create new teachers', (done) => {
+
+    });
+
+    it('should successfully lookup existing teachers', (done) => {
+
+    });
+
+    it('should update existing teachers', (done) => {
+
+    });
+
+    it('should remove teachers from the database', (done) => {
+
+    });
+  });
+
+  describe('Courses', function() {
+    it('should create new courses', (done) => {
+
+    });
+
+    it('should successfully lookup existing courses', (done) => {
+
+    });
+
+    it('should update existing courses', (done) => {
+
+    });
+
+    it('should remove courses from the database', (done) => {
+
+    });
+  });
+
+  describe('Announcements', function() {
+    it('should create new announcements', (done) => {
+
+    });
+
+    it('should successfully lookup existing announcements', (done) => {
+
+    });
+
+    it('should update existing announcements', (done) => {
+
+    });
+
+    it('should remove announcements from the database', (done) => {
+
+    });
+  });
+
+  describe('Assignments', function() {
+    it('should create new assignments', (done) => {
+
+    });
+
+    it('should successfully lookup existing assignments', (done) => {
+
+    });
+
+    it('should update existing assignments', (done) => {
+
+    });
+
+    it('should remove assignments from the database', (done) => {
+
+    });
+  });
+
+  describe('Submissions', function() {
+    it('should create new submissions', (done) => {
+
+    });
+
+    it('should successfully lookup existing submissions', (done) => {
+
+    });
+
+    it('should update existing submissions', (done) => {
+
+    });
+
+    it('should remove submissions from the database', (done) => {
+
+    });
+  });
+
+  describe('Student Courses & Grades', function() {
+    describe('Enrolling a Student', function() {
+      it('should add a student to a course', (done) => {
+
+      });
+
+      it('should initialize a student\'s grade to 100%', (done) => {
+
+      });
+    });
+
+    describe('Withdrawing a Student', function() {
+      it('should remove a student from a course', (done) => {
+
+      });
+    });
+  });
+});
