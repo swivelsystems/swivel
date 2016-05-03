e7b97471b66c942405427bee8a4dce0dffa875d9
CodeLizards <ebharris93@gmail.com>
2016-05-03 14:41:06 -0700

    Add home and class container components

--- client/components/ClassView/ClassContainer.jsx
+++ client/components/ClassView/ClassContainer.jsx
@@ -11,4 +11,3 @@ const ClassContainer = () => (
 );
 
 export default ClassContainer;
-
--- client/components/HomeView/HomeContainer.jsx
+++ client/components/HomeView/HomeContainer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import TimeLine from './Timeline.jsx';
 
 const HomeContainer = () => (
+import TimeLine from './homeView/Timeline.jsx';
+
   <div className="col-md-9 container">
     I am Home.
     <TimeLine />
