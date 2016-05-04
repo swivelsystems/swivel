c189bd138c9a54756c64ea54d0e35fc320516610
Joel Aguero <joelmaguero@gmail.com>
2016-05-04 16:47:22 -0700

    Add password var back

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 import password from '../config/mysqlsetup.js';
 
-const db = new Sequelize('swivel', 'root', '');
+const db = new Sequelize('swivel', 'root', password);
 
 export default db;
