40e476d430753b84141e7fbfc5403891b6212255
Joel Aguero <joelmaguero@gmail.com>
2016-05-03 18:37:00 -0700

    Fix styling errors

--- server/tests/models/Teacher.spec.js
+++ server/tests/models/Teacher.spec.js
@@ -1,21 +1,17 @@
 describe('Teacher Model', function() {
   it('should create new teachers', (done) => {
-
     done();
   });
 
   it('should successfully lookup existing teachers', (done) => {
-
     done();
   });
 
   it('should update existing teachers', (done) => {
-
     done();
   });
 
   it('should remove teachers from the database', (done) => {
-
     done();
   });
 });
