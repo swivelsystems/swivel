38e0b822e9594e9b8eee2c742c3303130eddd34d
Joel Aguero <joelaguero@users.noreply.github.com>
2016-05-03 17:36:55 -0700

    feat / Implement half db and index (resolves #36)
    * Implement Announcement schema
    
    * Implement Assignment schema
    
    * Implement Course schema
    
    * Configure table relationships
    
    * Switch export syntax to ES6

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,27 @@
 import db from 'db';
-// TODO: import all models here
+import { Announcement, Assignment, Course, Student, Submission, Teacher } from '../models';
 
 module.exports = function initBackend() {
+  Announcement.belongsTo(Teacher);
+  Announcement.belongsTo(Course);
 
-  // TODO: connect all models and specify relationships
+  Assignment.belongsTo(Course);
+  Assignment.hasMany(Submission);
 
-  // EXAMPLES -----------------------------------------
-  // Place.belongsToMany(User, {through: 'UserPlace'});
-  // User.belongsToMany(Place, {through: 'UserPlace'});
+  Course.belongsTo(Teacher);
+  Course.hasMany(Assignment);
+  Course.hasMany(Announcement);
+  Course.hasMany(Student, { through: 'StudentCourseGrade' });
+  // TODO: Figure out how to add a grade field to the StudentCourseGrade join table
+
+  Student.hasMany(Course, { through: 'StudentCourseGrade' });
+  Student.hasMany(Submission);
+
+  Submission.belongsTo(Student);
+  Submission.belongsTo(Assignment);
+
+  Teacher.hasMany(Course);
+  Teacher.hasMany(Announcement);
 
   // [modelName].sync(); //use {force: true} option to drop existing tables
   db.sync(); // Using this instead of syncing separately creates the join tables
