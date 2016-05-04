35c769aa5f1d20ee42fbccf18050cacddec58f43
Joel Aguero <joelmaguero@gmail.com>
2016-05-04 11:58:11 -0700

    Refactor tests to use req, res

--- tests/server/controllers/announcements.tests.js
+++ tests/server/controllers/announcements.tests.js
@@ -6,6 +6,7 @@ describe('Announcement Controller', () => {
   //   done();
   // });
 
+<<<<<<< bbe95dac79a4b3bb7812b745d688dd3f11edc539
   // it('should successfully retrieve existing announcements', (done) => {
   //   new Promise((resolve) => {
   //     resolve(announcements.findAllByCourse(1));
@@ -20,6 +21,24 @@ describe('Announcement Controller', () => {
   //     throw new Error(err);
   //   });
   // });
+=======
+  it('should successfully retrieve existing announcements', (done) => {
+    const req = { body: { courseId: 1 } };
+    new Promise((resolve) => {
+      resolve(announcements.findAllByCourse(req, {}));
+    })
+    .then((res) => {
+      const result = JSON.parse(res).body.announcements;
+      expect(result.length).toBeGreaterThan(0);
+      expect(result[0].body.length).toBeGreaterThan(0);
+      expect(result[0].title.length).toBeGreaterThan(0);
+      done();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+  });
+>>>>>>> Refactor tests to use req, res
 
   // it('should update existing announcements', (done) => {
   //   done();
--- tests/server/controllers/assignments.tests.js
+++ tests/server/controllers/assignments.tests.js
@@ -1,10 +1,11 @@
-import { assignments } from '../../../server/controllers/assignments.js';
+import * as assignments from '../../../server/controllers/assignments.js';
 
 describe('Assignment Controller', () => {
   // it('should create new assignments', (done) => {
   //   done();
   // });
 
+<<<<<<< bbe95dac79a4b3bb7812b745d688dd3f11edc539
   // it('should successfully retrieve existing assignments', (done) => {
   //   new Promise((resolve) => (
   //     resolve(assignments.findAllByCourse(1))
@@ -22,6 +23,26 @@ describe('Assignment Controller', () => {
   //   });
   // });
 
+=======
+  it('should successfully retrieve existing assignments', (done) => {
+    const req = { body: { courseId: 1 } };
+    new Promise((resolve) => (
+      resolve(assignments.findAllByCourse(req, {}))
+    ))
+    .then((res) => {
+      const result = JSON.parse(res).body.assignments;
+      expect(result.length).toBeGreaterThan(0);
+      expect(result[0].name.length).toBeGreaterThan(0);
+      expect(result[0].description.length).toBeGreaterThan(0);
+      expect(result[0].weight).toBeDefined();
+      expect(result[0].dueDate).toBeDefined();
+      done();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+  });
+>>>>>>> Refactor tests to use req, res
 
   // it('should update existing assignments', (done) => {
   //   done();
