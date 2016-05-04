02110441d2071287ef5bd3191265c95678495dcc
Joel Aguero <joelmaguero@gmail.com>
2016-05-03 17:05:37 -0700

    Implement Assignment schema

---
+++ server/models/Assignment.js
@@ -0,0 +1,33 @@
+import Sequelize from 'sequelize';
+import db from '../db/db.js';
+
+const Assignment = db.define('Assignment',
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV1,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      field: 'name',
+    },
+    description: {
+      type: Sequelize.STRING,
+      field: 'description',
+    },
+    weight: {
+      type: Sequelize.DECIMAL(5, 2), // 5 is max digits, e.g. 999.99, 2 is # dec places
+      field: 'weight',
+    },
+    dueDate: {
+      type: Sequelize.DATE,
+      field: 'due_date',
+    },
+  },
+  {
+    freezeTableName: true, // Model tableName will be the same as the model name
+  }
+);
+
+module.exports = Assignment;
