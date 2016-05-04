ea8183c6af570f36af9aac3346ed2ce946e533bb
Joel Aguero <joelmaguero@gmail.com>
2016-05-04 10:49:14 -0700

    Implement assigments controller unit test

--- tests/server/controllers/assignments.tests.js
+++ tests/server/controllers/assignments.tests.js
@@ -6,7 +6,20 @@ describe('Assignment Controller', () => {
   // });
 
   it('should successfully retrieve existing assignments', (done) => {
-    done();
+    new Promise((resolve) => (
+      resolve(assignments.findAllByCourse(1))
+    ))
+    .then((result) => {
+      expect(result.length).toBeGreaterThan(0);
+      expect(result[0].name.length).toBeGreaterThan(0);
+      expect(result[0].description.length).toBeGreaterThan(0);
+      expect(result[0].weight).toBeDefined();
+      expect(result[0].dueDate).toBeDefined();
+      done();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
   });
 
   // it('should update existing assignments', (done) => {
