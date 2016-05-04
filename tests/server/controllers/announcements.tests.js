116637d58896444987d992f74ca27599b94ff466
Joel Aguero <joelmaguero@gmail.com>
2016-05-03 23:03:29 -0700

    Write test for announcement controller

--- tests/server/controllers/announcements.tests.js
+++ tests/server/controllers/announcements.tests.js
@@ -5,20 +5,28 @@ describe('Announcement Controller', () => {
   //   done();
   // });
 
-  // it('should successfully retrieve existing announcements', (done) => {
-  //   new Promise((resolve) => {
-  //     resolve(announcements.findAllByCourse(1));
-  //   })
-  //   .then((result) => {
-  //     expect(result.length).toBeGreaterThan(0);
-  //     expect(result[0].body.length).toBeGreaterThan(0);
-  //     expect(result[0].title.length).toBeGreaterThan(0);
-  //     done();
-  //   })
-  //   .catch((err) => {
-  //     throw new Error(err);
-  //   });
-  // });
+  it('should successfully retrieve existing announcements', (done) => {
+    new Promise((res) => {
+      res(announcements.findAllByCourse(1));
+    })
+    .then((allFound) => {
+      expect(() => (
+        allFound.length > 0
+      )).toEqual(true);
+
+      expect(() => (
+        allFound[0].body.length > 0
+      )).toEqual(true);
+
+      expect(() => (
+        allFound[0].title.length > 0
+      )).toEqual(true);
+      done();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+  });
 
   // it('should update existing announcements', (done) => {
   //   done();
