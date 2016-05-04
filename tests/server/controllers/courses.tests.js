44bb94bfa2dde2fc1071ca079f046bda079ae6da
Joel Aguero <joelmaguero@gmail.com>
2016-05-04 11:00:29 -0700

    Implement course controller unit tests

--- tests/server/controllers/courses.tests.js
+++ tests/server/controllers/courses.tests.js
@@ -5,8 +5,38 @@ describe('Course Controller', () => {
   //   done();
   // });
 
-  it('should successfully retrieve existing courses', (done) => {
-    done();
+  describe('Retrieving courses', () => {
+    it('should retrieve courses for a student', (done) => {
+      new Promise((resolve) => (
+        resolve(courses.findAllByStudent(1))
+      ))
+      .then((result) => {
+        expect(result.length).toBeGreaterThan(0);
+        expect(result[0].name).toBeDefined();
+        expect(result[0].description).toBeDefined();
+        expect(result[0].department).toBeDefined();
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+      done();
+    });
+
+    it('should retrieve courses for a teacher', (done) => {
+      new Promise((resolve) => (
+        resolve(courses.findAllByTeacher(1))
+      ))
+      .then((result) => {
+        expect(result.length).toBeGreaterThan(0);
+        expect(result[0].name).toBeDefined();
+        expect(result[0].description).toBeDefined();
+        expect(result[0].department).toBeDefined();
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+      done();
+    });
   });
 
   // it('should update existing courses', (done) => {
