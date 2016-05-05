558d1b51a80719ff892ee16d94f34638465a4797
Joel Aguero <joelmaguero@gmail.com>
2016-05-05 15:11:48 -0700

    Update entry point for style processing

--- client/index.jsx
+++ client/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import styles from './stylesheets/styles.js';
+import styles from './styles/entry.js';
 
 const store = configureStore();
 
