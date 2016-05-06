af87dce888190a951497cf98865c7f67fa5fe13d
Joel Aguero <joelmaguero@gmail.com>
2016-05-05 20:38:02 -0700

    Add Swivel logo

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,6 +4,7 @@ const Nav = ({}) => (
 
   <nav className="nav-bar navbar navbar-default">
       <div className="navbar-header">
+        <div className="navbar-logo"></div>
         <a className="navbar-brand" href="#">
           Swivel Systems
         </a>
