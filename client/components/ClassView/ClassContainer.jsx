e4c60449ca1927286b65b62015e7a2707f0646c7
CodeLizards <ebharris93@gmail.com>
2016-05-10 11:36:35 -0700

    took barChart out of view

--- client/components/ClassView/ClassContainer.jsx
+++ client/components/ClassView/ClassContainer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Chart from './Chart.jsx';
+import Chart from './Chart.jsx'; 
 import Info from './Info.jsx';
 
 const ClassContainer = () => (
   <div className="course card">
-    I am in ClassContainer.
-    <Chart />
     <Info />
   </div>
 );
