c1c28bf19b316841e7d57ccff9b47451ada0d01f
CodeLizards <ebharris93@gmail.com>
2016-05-10 16:53:30 -0700

    Add button on navbar that toggles between student and teacher view, changes demoType state, and displays other demo on button

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import actions from '../actions/index.js';
+import { connect } from 'react-redux';
 
-const Nav = () => (
-  <nav className="navbar navbar-default">
-      <div className="navbar-header">
-        <div className="navbar-logo"></div>
-        <a className="navbar-brand" href="#">
-          Swivel Systems
-        </a>
-      </div>
-  </nav>
+class Nav extends Component {
+  handleClick() {
+    let type = '';
+    if (this.props.demoType === 'Student') {
+      type = 'Teacher';
+    } else {
+      type = 'Student';
+    }
+    this.props.handleDemoToggle(type);
+  }
+
+  displayOtherDemoType() {
+    let otherDemo = '';
+    if (this.props.demoType === 'Student') {
+      otherDemo = 'Teacher';
+    } else {
+      otherDemo = 'Student';
+    }
+    return otherDemo;
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+          <div className="navbar-header">
+            <div className="navbar-logo"></div>
+            <a className="navbar-brand" href="#">
+              Swivel Systems
+            </a>
+          </div>
+            <a
+              className="btn nav-btn"
+              onClick={ this.handleClick.bind(this) }
+            >
+            Try { this.displayOtherDemoType() } Demo!
+            </a>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  { demoType: state.demoType }
 );
 
-export default Nav;
+const mapDispatchToProps = (dispatch) => (
+  {
+    handleDemoToggle: (demoType) => {
+      dispatch(actions.toggleDemoType(demoType));
+    },
+  }
+);
+
+Nav.propTypes = {
+  demoType: PropTypes.string,
+  handleDemoToggle: PropTypes.func,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Nav);
