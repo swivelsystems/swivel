7a4a34516e6c5b87c6d9478e3a44be271bfb1200
Joel Aguero <joelmaguero@gmail.com>
2016-05-10 20:07:01 -0700

    Implement announcement rendering and cards on home

--- client/components/HomeView/Announcement.jsx
+++ client/components/HomeView/Announcement.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const Announcement = () => (
-  <div>This is an annnouncement.</div>
+const Announcement = (props) => (
+  <div className="announcement">
+    <div className="announcement-label">Announcement</div>
+    <h4>{ props.content.title }</h4>
+    <p>{ props.content.body }</p>
+  </div>
 );
+
+Announcement.propTypes = {
+  content: PropTypes.object,
+};
 
 export default Announcement;
