28afecd9a3250be9883983e919d3edb85771e6eb
CodeLizards <ebharris93@gmail.com>
2016-05-12 17:16:26 -0700

    Update toggle views to have bar graph for assignments to switch to doughnut charts on students tabs, change info for doughtnut charts on student/teacher view, take away students tab for teacher view

--- client/components/ClassView/BarChart.jsx
+++ client/components/ClassView/BarChart.jsx
@@ -2,29 +2,15 @@ import React, { Component } from 'react';
 import ChartJS from 'chart.js';
 import ReactDOM from 'react-dom';
 import { Bar } from 'react-chartjs';
+import * as ChartOptions from '../../constants/ChartOptions.js';
 
 class BarChart extends Component {
-
   render() {
-    const barData = {
-      labels: ['h', 'e', 'l', 'l', 'o', 'o', 'o'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: '#3498db',
-          color: '#3498db',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [65, 99, 80, 81, 86, 80, 79, 65, 99, 80, 81, 86, 80, 79, 65, 99, 80, 81, 86, 80, 79],
-        },
-      ],
-    };
-
     return (
-      <div>
-        <div>Assignment Grades</div>
-        <Bar data={barData} />
+      <div className="bar-container">
+        <h5>Averall Performance on Assignments</h5>
+        <Bar data={ChartOptions.barData} options={ChartOptions.barChartOptions} />
+        <Bar data={ChartOptions.barData} options={ChartOptions.barChartOptions} />
       </div>
     );
   }
