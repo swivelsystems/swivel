3ec98ba315e8415d305ed1e23269963847ce83a5
CodeLizards <ebharris93@gmail.com>
2016-05-12 17:21:55 -0700

    add function to display charts according to tab and which demo view

--- client/components/ClassView/CourseInfo.jsx
+++ client/components/ClassView/CourseInfo.jsx
@@ -5,24 +5,56 @@ import AllStudentsTab from './AllStudentsTab.jsx';
 import AssignmentsTab from './AssignmentsTab.jsx';
 import Student from './Student.jsx';
 import Assignment from './Assignment.jsx';
+import DoughnutChart from './DoughnutChart.jsx';
+import BarChart from './BarChart.jsx';
 
 
 class CourseInfo extends Component {
   // depending on which tab is clicked, render Assignments, AllStudents, or Student component
   handleTabs() {
     if (this.props.tabView === 'Students') {
-      return <AllStudentsTab />;
+      return (
+        <AllStudentsTab />
+      );
     } else if (this.props.tabView === 'Student') {
       return <Student />;
     } else if (this.props.tabView === 'Assignment') {
       return <Assignment />;
     }
-    return <AssignmentsTab />;
+    return (
+      <AssignmentsTab />
+    );
+  }
+
+  handleCharts() {
+    if (this.props.demoType === 'student' || this.props.tabView === 'Students') {
+      return <DoughnutChart />;
+    }
+    return <BarChart />;
+  }
+
+  displayStudentsTab() {
+    let tab = '';
+    if (this.props.demoType === 'teacher') {
+      tab = (
+        <li
+          role="presentation"
+          className="course-info-nav-tab students-tab"
+          onClick={ () => this.props.handleTab('Students') }
+        >
+          <a>View Students</a>
+        </li>
+      );
+    }
+    return tab;
   }
 
   render() {
     return (
       <div className="course-info">
+        <div>
+          {this.handleCharts()}
+        </div>
         <ul className="course-info-nav nav nav-tabs">
           <li
             role="presentation"
@@ -31,13 +63,7 @@ class CourseInfo extends Component {
           >
             <a>View Assignments</a>
           </li>
-          <li
-            role="presentation"
-            className="course-info-nav-tab students-tab"
-            onClick={ () => this.props.handleTab('Students') }
-          >
-            <a>View Students</a>
-          </li>
+          { this.displayStudentsTab() }
         </ul>
         <div className="course-info-content">
           { this.handleTabs() }
@@ -48,7 +74,10 @@ class CourseInfo extends Component {
 }
 
 const mapStateToProps = (state) => (
-  { tabView: state.tabView }
+  {
+    tabView: state.tabView,
+    demoType: state.demoType,
+  }
 );
 
 const mapDispatchToProps = (dispatch) => (
@@ -62,6 +91,7 @@ const mapDispatchToProps = (dispatch) => (
 CourseInfo.propTypes = {
   tabView: PropTypes.string,
   handleTab: PropTypes.func,
+  demoType: PropTypes.string,
 };
 
 export default connect(
