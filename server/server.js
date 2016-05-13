a41c924676f5c2fdf7f2e11565f22d04fad3097e
Kevin E Meraz <kevinemeraz@gmail.com>
2016-05-13 12:00:03 -0700

    feat / Implement compression for incoming requests

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+// import passport from 'passport';
+import morgan from 'morgan';
+import compression from 'compression';
 import studentsRouter from './routers/studentsRouter.js';
 import teachersRouter from './routers/teachersRouter.js';
-import passport from 'passport';
-import morgan from 'morgan';
-import session from 'express-session';
 
-const authenticate = require('./controllers/auth.js').authenticate;
-const LocalStrategy = require('passport-local').Strategy;
+// const authenticate = require('./controllers/auth.js').authenticate;
+// const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(compression());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -20,16 +21,16 @@ app.use((req, res, next) => {
 app.use('/api/students', studentsRouter);
 app.use('/api/teachers', teachersRouter);
 
-app.use(session({ secret: 'keyboard cat' }));
+// app.use(session({ secret: 'keyboard cat' }));
 app.use(morgan('dev'));
 
 app.use(express.static(`${__dirname}/../client/`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-}, authenticate));
+// passport.use(new LocalStrategy({
+//   usernameField: 'email',
+// }, authenticate));
 
 app.listen(port, (err) => {
   if (err) { throw new Error(err); }
