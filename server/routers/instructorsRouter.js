c7427f1f3359da62908f1e03446e365c0ba596c2
Jonathen Chen <jonathen0915@gmail.com>
2016-05-14 12:33:59 -0700

    Complete homepage API end Point with speed optimzation through async waterfall and simplified package object

--- server/routers/instructorsRouter.js
+++ server/routers/instructorsRouter.js
@@ -2,8 +2,11 @@ import express from 'express'
 import * as instructorsService from '../services/instructorsService.js';
 const instructorsRouter = express.Router();
 
-instructorsRouter.route('/courses/:id')
-  .get(instructorsService.retrieveCourses);
+instructorsRouter.route('/home/:id')
+  .get(instructorsService.retrieveHome);
+
+instructorsRouter.route('/course/:id')
+  .get(instructorsService.retrieveCourse);
 
 export default instructorsRouter;
 
--- server/services/instructorsService.js
+++ server/services/instructorsService.js
@@ -5,14 +5,54 @@ import * as Assignments from '../controllers/assignments.js';
 import * as Submissions from '../controllers/submissions.js';
 import * as Students from '../controllers/students.js';
 
-export const retrieveCourses = (req, res) => {
+export const retrieveHome = (req, res) => {
   const instructorId = req.params.id;
 
-  const package = {};
+  let homePackage = {}
 
-  Teachers.findById(teacherId)
-    .then((id) => {
+  Courses.findAllByTeacher(instructorId)
+    .then((courses) => {
 
+      homePackage.courses = courses;
+
+      const arrOfCoursePromises = homePackage.courses.map((course) => {
+        return Announcements.findAllByCourse(course.id)
+          .then((announcements) => {
+            course.announcements = announcements;
+          })
+          .catch((err) => {
+            console.err('failed at finding announcements', err)
+            res.status(500).send('failed at finding announcements for', course.name);
+          })
+          .then(() =>{
+            return Assignments.findAllByCourse(course.id)
+          })
+          .catch((err) => {
+            console.err('failed at finding assignments', err)
+            res.status(500).send('failed at finding assignments for', course.name);
+          })
+          .then((assignments) => {
+            course.assignments = assignments;
+          })
+      })
+      return Promise.all(arrOfCoursePromises)
     })
+    .catch((err) => {
+      console.err("can't find teacher", err)
+      res.status(500).send('failed at finding teacher: ', instructorId);
+    })
+    .then((arrOfCoursePromises) => {
+      res.send(homePackage);
+    })
+    .catch((err) => {
+      console.err("can't compile courses", err)
+      res.status(500).send('failed at compiling all data for teacher', instructorId);
+    });
+}
+
+export const retrieveCourse = (req, res) => {
+  const courseId = req.params.id;
+
+  let coursePackage = {}
 
 }
