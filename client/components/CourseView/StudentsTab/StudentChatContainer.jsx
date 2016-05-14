675c57c329e7fa66bc8e1a180170c4a85fff009c
Joel Aguero <joelmaguero@gmail.com>
2016-05-14 15:13:47 -0700

    Setup socket emitters and listeners in chat container

--- client/components/CourseView/StudentsTab/StudentChatContainer.jsx
+++ client/components/CourseView/StudentsTab/StudentChatContainer.jsx
@@ -1,8 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 import actions from '../../../actions/index.js';
 import { connect } from 'react-redux';
+import socket from '../../../utils/socket.js';
 
 class StudentChatContainer extends Component {
+  constructor(props) {
+    super(props);
+    // open a socket connection and emit that the conversation was started
+    const teacherId = 5;
+    socket.emit('openChat', teacherId, this.props.displayedStudent.id, function(data) {
+      console.log(data); // render chat bubbles
+      // get the messages and update the ui with them
+    });
+    // add a socket listener for new messages from this student
+    socket.on('newMessage', function(data) {
+      console.log(data); // update the UI with the new message
+    });
+  }
+
+  componentWillUnmount() {
+    // close the socket connection and any listeners
+    const teacherId = 5;
+    socket.emit('closeChat', teacherId, this.props.displayedStudent.id, function(data) {
+      console.log(data);
+    });
+  }
   // when a single student is clicked
   // get the current student and display their info
   render() {
