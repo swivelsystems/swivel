5937d72194676dc29abf31076814fb23a902dc4c
Jonathen Chen <jonathen0915@gmail.com>
2016-05-16 10:55:05 -0700

    feat / Complete Socket.io integration
    Complete integration of sockets for server and modularized out the socket module
    into socket.js and created basic user tracker in socket.js module.
    
    Commits:
    - Connect sockets to server & comment server code
    - Modularize and initialize Sockets through socket.js file
    - Add emit templates and console logs for testing with mobile and web
    - Complete and tested connection between mobile client and server
    - Create login, authentication template, and logout for each individual user

--- server/server.js
+++ server/server.js
@@ -4,14 +4,25 @@ import morgan from 'morgan';
 import compression from 'compression';
 import studentsRouter from './routers/studentsRouter.js';
 import teachersRouter from './routers/teachersRouter.js';
+import * as sockets from './socket.js';
+
 // import passport from 'passport';
+// const authenticate = require('./controllers/auth.js').authenticate;
+// const LocalStrategy = require('passport-local').Strategy;
 
-const authenticate = require('./controllers/auth.js').authenticate;
-const LocalStrategy = require('passport-local').Strategy;
 
+//Express & Server declarations
 const app = express();
 const port = process.env.PORT || 8080;
+const server = app.listen(port, (err) => {
+  if (err) { throw new Error(err); }
+  console.log('Swivel server listening on port: ' + port);
+});
 
+//Sockets
+sockets.socketServer(app, server);
+
+//Server Configuration
 app.use(compression());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -22,6 +33,7 @@ app.use((req, res, next) => {
   next();
 });
 
+//Routing
 app.use('/api/students', studentsRouter);
 app.use('/api/teachers', teachersRouter);
 
@@ -32,18 +44,8 @@ app.use(express.static(`${__dirname}/../client/`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-}, authenticate));
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('a user disconnected');
-  });
-});
+// passport.use(new LocalStrategy({
+//   usernameField: 'email',
+// }, authenticate));
 
-http.listen(port, (err) => {
-  if (err) { throw new Error(err); }
-  console.log('Swivel server listening on port: ' + port);
-});
