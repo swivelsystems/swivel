ed5c01a8c0f82feaed601befc17899d705f87092
CodeLizards <ebharris93@gmail.com>
2016-05-18 15:25:36 -0700

    implement controller to save chats to database

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -2,17 +2,12 @@ import Chat from '../models/Chat.js';
 
 export const saveChats = (chats, teacher, student) => {
   for (let i = 0; i < chats.length; i++) {
-    return new Promise((resolve, reject) => {
-      Chat.create({
-        authorId: chats[i].author,
-        timestamp: chats[i].timestamp,
-        teacherId: teacher.id,
-        studentId: student.id,
-        body: chats[i].body,
-      })
-      .catch((err) => {
-        reject(err);
-      });
+    Chat.create({
+      authorId: chats[i].author,
+      timestamp: chats[i].timestamp,
+      teacherId: teacher.id,
+      studentId: student.id,
+      body: chats[i].body,
     });
   }
 };
