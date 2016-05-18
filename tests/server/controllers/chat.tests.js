006b0e32e470a9fffac1faa254c48866a72634f1
CodeLizards <ebharris93@gmail.com>
2016-05-18 15:46:34 -0700

    wrote shells for testing redis database code

---
+++ tests/server/controllers/chat.tests.js
@@ -0,0 +1,7 @@
+import { chat } from '../../../server/controllers/chat.js';
+
+describe('redisChat Controller', () => {
+  it('should return list messages if they are successfully put into db', (done) => {
+    done();
+  });
+});
