abe7c608a446ad6b333efaa3a0be6553fbb47a47
Kevin E Meraz <kevinemeraz@gmail.com>
2016-05-19 14:22:01 -0700

    Implement AWS ElastiCache-facing aspect of chat functionality

--- server/db/messagesStore.js
+++
@@ -1,5 +0,0 @@
-import redis from 'redis';
-export default redis.createClient();
-
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, () =>() { /* ... */ });
--- server/db/redis.js
+++ server/db/redis.js
@@ -1,8 +1,11 @@
 import redis from 'redis';
 
 const port = 6379;
-const readEnpoint = 'swivelredis-001.kxlall.0001.usw2.cache.amazonaws.com';
-const primaryEndpoint = 'swivelredis.kxlall.ng.0001.usw2.cache.amazonaws.com';
+const sqlReadEndpoint = 'swivelredis-001.kxlall.0001.usw2.cache.amazonaws.com';
+const sqlPrimaryEndpoint = 'swivelredis.kxlall.ng.0001.usw2.cache.amazonaws.com';
+
+const chatReadEndpoint = 'swivelchat-001.kxlall.0001.usw2.cache.amazonaws.com';
+const chatPrimaryEndpoint = 'swivelchat.kxlall.ng.0001.usw2.cache.amazonaws.com';
 
 // export const readURI = redis.createClient({
 //   retry_strategy: (options) => {
@@ -41,7 +44,7 @@ const primaryEndpoint = 'swivelredis.kxlall.ng.0001.usw2.cache.amazonaws.com';
 //   },
 // });
 
-export const readURI = redis.createClient(port, readEnpoint, {
+export const sqlReadURI = redis.createClient(port, sqlReadEndpoint, {
   retry_strategy: (options) => {
     if (options.error.code === 'ECONNREFUSED') {
       // End reconnecting on a specific error and flush all commands with a individual error
@@ -60,7 +63,45 @@ export const readURI = redis.createClient(port, readEnpoint, {
   },
 });
 
-export const writeURI = redis.createClient(port, primaryEndpoint, {
+export const sqlWriteURI = redis.createClient(port, sqlPrimaryEndpoint, {
+  retry_strategy: (options) => {
+    if (options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with a individual error
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands with a individual error
+      return new Error('Retry time exhausted');
+    }
+    if (options.times_connected > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.max(options.attempt * 100, 3000);
+  },
+});
+
+export const chatReadURI = redis.createClient(port, chatReadEndpoint, {
+  retry_strategy: (options) => {
+    if (options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with a individual error
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands with a individual error
+      return new Error('Retry time exhausted');
+    }
+    if (options.times_connected > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.max(options.attempt * 100, 3000);
+  },
+});
+
+export const chatWriteURI = redis.createClient(port, chatPrimaryEndpoint, {
   retry_strategy: (options) => {
     if (options.error.code === 'ECONNREFUSED') {
       // End reconnecting on a specific error and flush all commands with a individual error
