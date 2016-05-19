00eec4d4b94843fd3154900caa02ea3c2bcd3dfa
CodeLizards <ebharris93@gmail.com>
2016-05-18 20:41:14 -0700

    wrote bare bones for testing redis. comment out until complete

--- tests/server/controllers/chat.tests.js
+++ tests/server/controllers/chat.tests.js
@@ -1,7 +1,7 @@
-import { chat } from '../../../server/controllers/chat.js';
+// import { chat } from '../../../server/controllers/chat.js';
 
-describe('redisChat Controller', () => {
-  it('should return list messages if they are successfully put into db', (done) => {
-    done();
-  });
-});
+// describe('redisChat Controller', () => {
+//   it('should return list messages if they are successfully put into db', (done) => {
+//     done();
+//   });
+// });
--- tests/server/controllers/redisChat.tests.js
+++ tests/server/controllers/redisChat.tests.js
@@ -1,15 +1,15 @@
-import { chat } from '../../../server/controllers/redisControllers/chat.js';
+// import { chat } from '../../../server/controllers/redisControllers/chat.js';
 
-describe('redisChat Controller', () => {
-  it('should return greater than one if message stored in redis', (done) => {
-    done();
-  });
+// describe('redisChat Controller', () => {
+//   it('should return greater than one if message stored in redis', (done) => {
+//     done();
+//   });
 
-  it('should return all of the users messages in the redis db', (done) => {
-    done();
-  });
+//   it('should return all of the users messages in the redis db', (done) => {
+//     done();
+//   });
 
-  it('should invoke saveChats in mysql chat controller after 50 msgs per user', (done) => {
-    done();
-  });
-});
+//   it('should invoke saveChats in mysql chat controller after 50 msgs per user', (done) => {
+//     done();
+//   });
+// });
