c55d8fb25501ab17f3f20437e318f6517d255306
CodeLizards <ebharris93@gmail.com>
2016-05-18 20:38:32 -0700

    update redis controllers to write data to redis and get messages from redis but abstract out writing to persistent db

--- server/controllers/redisControllers/chat.js
+++ server/controllers/redisControllers/chat.js
@@ -3,6 +3,10 @@ import client from '../../db/messagesStore.js';
 import * as chats from '../chats.js';
 
 
+// anticipated sender, recipient, message format
+// sender = {id: 10, type:'teacher'};
+// recipient = {id: 20, type: 'teacher'};
+// message = {body: 'hey whats up?', timestamp: '10:55'};
 const teacherOrStudent = (sender, recipient) => {
   let teacher;
   let student;
@@ -24,28 +28,6 @@ export const sendMessage = (sender, recipient, message) => {
     console.log('Error connecting to redis server in sendMessage ', err)
   ));
 
-
-  // pop chats from redis and move to persisitent db once a chat between users has 50 msgs
-  client.llen(`${teacher}and${student}`, (error1, reply) => {
-    if (error1) {
-      console.log('there was an error getting the length of ${teacher}and${student}', error1);
-    }
-    if (reply > 50) {
-      client.lrange(`${teacher}and${student}`, 0, 50, (error2, replies) => {
-        if (error2) {
-          console.log('error in getting all chats in from range 0-50 in sendMessage', error2);
-        }
-        chats.saveChats(replies.slice(0, 50), teacher, student);
-      });
-      for (let i = 0; i < 50; i++) {
-        client.rpop(`${teacher}and${student}`, (error3) => {
-          if (error3) {
-            console.log('error popping off ${teacher}and${student} list');
-          }
-        });
-      }
-    }
-  });
   // put sent message into redis store
   client.lpush(`${teacher}and${student}`, chat, (err, reply) => {
     if (err) {
@@ -55,20 +37,20 @@ export const sendMessage = (sender, recipient, message) => {
   });
 };
 
-export const getUserMessages = (sender, recipient) => {
+export const getUserMessages = (sender, recipient, callback) => {
   const [teacher, student] = teacherOrStudent(sender, recipient);
 
   client.on('error', (err) => (
     console.log('Error connecting to redis server in getUserMessages ', err)
   ));
-  client.lrange(`${teacher}and${student}`, 0, 10, (err, replies) => {
+  client.lrange(`${teacher}and${student}`, 0, 100, (err, replies) => {
     if (err) {
       return console.log('Error in getUserMessages', err);
     }
     if (replies === 0) {
       return console.log('This student has no chat history!');
     }
-    return replies;
+    callback(replies);
   });
 };
 
