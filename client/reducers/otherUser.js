bb392b970f12531bef6ee1de2fe3afb85fb409f2
Joel Aguero <joelmaguero@gmail.com>
2016-05-19 18:32:11 -0700

    Remove console log

--- client/reducers/otherUser.js
+++ client/reducers/otherUser.js
@@ -5,7 +5,6 @@ const initialState = {};
 export default function otherUser(state = initialState, action) {
   switch (action.type) {
     case types.UPDATE_OTHER_USER:
-      console.log('other user will be...', action.user);
       return action.user;
     default:
       return state;
